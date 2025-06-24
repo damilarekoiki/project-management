72ab3e58311add35dd321c67455047c1d218f206
Damilare Koiki <koikidamilare@gmail.com>
2025-06-24 12:17:12 +0100

    setup: setup app behaviour; setup test tools; install axios; set pre-commit commands

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +25,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Model::automaticallyEagerLoadRelationships(true);
+        Date::use(CarbonImmutable::class);
+        DB::prohibitDestructiveCommands(app()->isProduction());
+        // Force HTTPS in production
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
